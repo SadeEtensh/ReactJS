ae77ac953ea49ca10db6beade3c9a798c89695c7
SadeEtensh <eteneshsade550305@gmail.com>
2024-01-17 15:54:22 +0300

    completed nice design of card componenet

---
+++ src/comps/Card.jsx
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <div className="card">
+      <img
+        className="card-image"
+        src="https://media.licdn.com/dms/image/D4E03AQGUYQ27o5-v6Q/profile-displayphoto-shrink_800_800/0/1682192871664?e=1710979200&v=beta&t=NwCgTwWcu9tXvRhYI3LSBjJEH44YXp5KkRvHHd-KnG4"
+        alt="Profile Picture"
+      />
+      <h2 className="card-title">Sade The App</h2>
+      <p className="card-text">I'am Sr Application Engineer</p>
+    </div>
+  );
+}
+
+export default Card;
