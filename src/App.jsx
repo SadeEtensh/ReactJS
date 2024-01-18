c88b245436a87896cce8b97770b48e1333825306
SadeEtensh <eteneshsade550305@gmail.com>
2024-01-18 11:28:04 +0300

    styling React APP

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./comps/Card";
+import Button from "./Button/Button";
 function App() {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {/* <Card /> */}
+      <Button />
     </>
   );
 }
